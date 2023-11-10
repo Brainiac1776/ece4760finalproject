5d9602ac9abd272e675250204234b15e126358d2
Brainiac1776 <chimdianude24@gmail.com>
2023-11-10 14:28:35 -0500

    hardware connections

---
+++ project.c
@@ -0,0 +1,17 @@
+/*
+ * Michael Liang and Chimdi Anude
+ * Implementation of Pico Says
+ * 
+ * 
+ * HARDWARE CONNECTIONXD
+ * - GPIO 0 - Red LED
+ * - GPIP 1 - Red Push Button
+ * - GPIO 4 - Blue LED
+ * - GPIO 5 - Blue Push Button
+ * - GPIO 8 - Green LED
+ * - GPIO 9 - Green Push Button
+ * - GPIO 12 - Yellow LED 
+ * - GPIO 13 - Yellow Push Button
+
+
+*/
