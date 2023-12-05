f6871da928a810b6584d4e2975209b0e50120239
Chimdindu Anude <68574930+Brainiac1776@users.noreply.github.com>
2023-12-05 16:07:13 -0500

    Update button.c

--- button.c
+++ button.c
@@ -1,6 +1,49 @@
 // button.c
 #include "button.h"
+// #include "RED.h"
+// #include "BLUE.h"
+// #include "GREEN.h"
+// #include "YELLOW.h"
 #include "pico/stdlib.h"
+
+//Send sound to the DAC 
+/*
+void redSound()
+{
+ for (int i = 0; i < sizeof(RED_wav)){
+     DAC_output_0 = i;
+     DAC_data_0 = DAC_config_channel B |  DAC_output_0 & 0xffff
+     spi_write16_blocking(SPI_PORT, &DACdata_0, 1)
+ }
+}
+
+void blueSound()
+{
+ for (int i = 0; i < sizeof(BLUE_wav)){
+     DAC_output_0 = i;
+     DAC_data_0 = DAC_config_channel B |  DAC_output_0 & 0xffff
+     spi_write16_blocking(SPI_PORT, &DACdata_0, 1)
+ }
+}
+
+void greenSound()
+{
+ for (int i = 0; i < sizeof(GREEN_wav)){
+     DAC_output_0 = i;
+     DAC_data_0 = DAC_config_channel B |  DAC_output_0 & 0xffff
+     spi_write16_blocking(SPI_PORT, &DACdata_0, 1)
+ }
+}
+
+void yellowSound()
+{
+ for (int i = 0; i < sizeof(YELLOW_wav)){
+     DAC_output_0 = i;
+     DAC_data_0 = DAC_config_channel B |  DAC_output_0 & 0xffff
+     spi_write16_blocking(SPI_PORT, &DACdata_0, 1)
+ }
+}
+*/
 
 // Initialize buttons
 void init_buttons()
@@ -27,15 +70,19 @@ void init_buttons()
 int check_buttons()
 {
     if (gpio_get(RED_BUTTON_PIN) == 0) {
+        //redSound();
         return 1;
     }
     if (gpio_get(GREEN_BUTTON_PIN) == 0){
+        //blueSound();
         return 2;
     }
     if (gpio_get(YELLOW_BUTTON_PIN) == 0){
+        //yellowSound();
         return 3;
     }
     if (gpio_get(BLUE_BUTTON_PIN) == 0){
+       // greenSound();
         return 4;
     }
 
